568f3dfacd1e86bb68aab96198a45cf7c01b9ffc
BhageshSM2005 <bhageshsm2005@gmail.com>
2026-06-30 16:29:56 +0530

    Initial project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Smart Real Estate Analytics Platform
+
+An AI-powered full-stack web application that predicts Bengaluru house prices using Machine Learning.
+
+## Tech Stack
+
+- Python
+- Flask
+- MySQL
+- React (Vite)
+- Tailwind CSS
+- Scikit-Learn
+- JWT Authentication
---
+++ ml/train_model.py
@@ -0,0 +1,39 @@
+import pickle
+import json
+import numpy as np
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+model_path = os.path.join(
+    BASE_DIR,
+    "house_price_model.pkl"
+)
+
+columns_path = os.path.join(
+    BASE_DIR,
+    "columns.json"
+)
+
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
+
+with open(columns_path, "r") as f:
+    data_columns = json.load(f)["data_columns"]
+
+def get_location_names():
+    return data_columns[3:]
+
+def predict_price(location, sqft, bath, bhk):
+
+    x = np.zeros(len(data_columns))
+
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+
+    if location.lower() in data_columns:
+        loc_index = data_columns.index(location.lower())
+        x[loc_index] = 1
+
+    return model.predict([x])[0]
